a7a965763273ddd60cb4a7de4181b47f9eb31570
Artur Zakharov <118992316+arthurzakharov@users.noreply.github.com>
2026-02-12 13:16:58 +0100

    Add annotration to content util component

--- src/utils/content/content.component.tsx
+++ src/utils/content/content.component.tsx
@@ -7,11 +7,12 @@ export interface ContentProps extends HTMLAttributes<HTMLElementTagNameMap> {
 }
 
 /**
- * Renders content that can be either a string (potentially containing HTML) or React nodes.
- * If the content is a string that contains HTML tags, it uses dangerouslySetInnerHTML to render it as HTML.
- * If the content is a plain string without HTML tags, it renders them directly.
- * If the content is React nodes, it renders them directly.
- * The 'alwaysRender' prop allows rendering an empty tag even if there are no children, which can be useful for layout purposes.
+ * Renders content that can be either a string (potentially containing HTML) or React nodes.<br>
+ * With **tag**, you can specify which HTML tag to use for rendering the content (default is *div*).<br>
+ * If the **content** is a string that contains HTML tags, it uses *dangerouslySetInnerHTML* to render it as HTML.<br>
+ * If the **content** is a plain string without HTML tags, it renders them directly.<br>
+ * If the **content** is React nodes, it renders them directly.<br>
+ * The **alwaysRender** prop allows rendering an empty tag even if there are no children, which can be useful for layout purposes.
  */
 export const Content = ({ tag = 'div', alwaysRender = false, children, ...rest }: ContentProps) => {
   if (!alwaysRender && !children) return null;
